e9ae0f7194cb96fd86ae2e4fbb5bf9815e14dbca
rabiaadel <rabiaadelgamal@gmail.com>
2025-09-30 19:18:40 +0300

    finally added terraform final changes

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -2,6 +2,9 @@ resource "aws_instance" "petclinic" {
   ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 us-east-1
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.private.id
+  #subnet_id = aws_subnet.public.id
+  #associate_public_ip_address = true
+
   key_name      = var.key_name
 
   vpc_security_group_ids = [aws_security_group.app_sg.id]
